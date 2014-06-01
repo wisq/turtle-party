059fc70a90d49b3d9b4112a1afbf3400f0fbb791
Adrian Irving-Beer <wisq@wisq.net>
2014-06-01 00:29:56 -0400

    Basic auto-update functionality.

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -1,2 +1,34 @@
 class UpdateController < ApplicationController
+  TURTLE_PATH = Rails.root + 'turtle'
+
+  def manifest
+    manifest_text = generate_manifest.map do |key, (size, hash)|
+      [key, size, hash].join(" ").to_s
+    end.join("\n")
+
+    render :text => manifest_text, :content_type => 'text/plain'
+  end
+
+  def file
+    path = (TURTLE_PATH + params[:path]).realpath
+
+    if path.to_s.start_with?(TURTLE_PATH.to_s + "/") && path.file?
+      send_file(path)
+    else
+      render :text => "File not found", :status => 404
+    end
+  end
+
+  private
+
+  def generate_manifest
+    manifest = {}
+    TURTLE_PATH.find do |file|
+      next unless file.file? || file.symlink?
+      path = file.relative_path_from(TURTLE_PATH)
+      digest = Digest::SHA1.hexdigest(file.read)
+      manifest[path.to_s] = [file.size, digest]
+    end
+    manifest
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/update/manifest', to: 'update#manifest'
+  get '/update/file', to: 'update#file'
+end
+
+__END__
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +58,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
